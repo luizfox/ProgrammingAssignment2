e9fe41553b386c17930275deda0beaaa0ccecefe
Luiz Felipe <luiz@ansia.cave>
2014-06-19 02:30:29 -0300

    first commit

---
+++ cachematrix.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...) {
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$getInverse()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
+}
